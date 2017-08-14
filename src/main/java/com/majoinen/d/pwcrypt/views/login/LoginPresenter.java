5cff81968cbd116dda4d84ab0dbcc8297058c233
Daniel Majoinen <daniel@majoinen.com>
2017-08-15 06:38:23 +1000

    Add basic login view

--- src/main/java/com/majoinen/d/pwcrypt/views/login/LoginPresenter.java
+++ src/main/java/com/majoinen/d/pwcrypt/views/login/LoginPresenter.java
@@ -2,9 +2,13 @@ package com.majoinen.d.pwcrypt.views.login;
 
 import com.gluonhq.charm.glisten.application.MobileApplication;
 import com.gluonhq.charm.glisten.mvc.View;
+import com.majoinen.d.pwcrypt.PwCrypt;
 import com.majoinen.d.pwcrypt.log.LogManager;
 import com.majoinen.d.pwcrypt.log.Logger;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class LoginPresenter {
 
@@ -14,10 +18,25 @@ public class LoginPresenter {
     @FXML
     private View view;
 
+    @FXML
+    private Button login;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private PasswordField password;
+
     public void initialize() {
-        // Hide the AppBar
-        view.setOnShowing(event ->
-          MobileApplication.getInstance().getAppBar().setVisible(false)
-        );
+        // Hide the AppBar & remove focus from email box
+        view.setOnShowing(event -> {
+            MobileApplication.getInstance().getAppBar().setVisible(false);
+            view.requestFocus();
+        });
+    }
+
+    @FXML
+    private void gotoRegister() {
+        MobileApplication.getInstance().switchView(PwCrypt.REGISTER_VIEW);
     }
 }
