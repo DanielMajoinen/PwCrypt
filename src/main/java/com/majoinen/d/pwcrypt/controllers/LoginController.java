be5694375de89bc45e373f023f378852f91179ca
Daniel Majoinen <daniel@majoinen.com>
2017-08-17 02:45:07 +1000

    Add ability to log in verifying credentials with the database

--- src/main/java/com/majoinen/d/pwcrypt/controllers/LoginController.java
+++ src/main/java/com/majoinen/d/pwcrypt/controllers/LoginController.java
@@ -1,6 +1,14 @@
 package com.majoinen.d.pwcrypt.controllers;
 
+import com.majoinen.d.database.DatabaseController;
+import com.majoinen.d.database.DatabaseControllerFactory;
+import com.majoinen.d.database.exception.DBUtilsException;
+import com.majoinen.d.encryption.exception.EncryptionUtilsException;
+import com.majoinen.d.pwcrypt.PwCrypt;
+import com.majoinen.d.pwcrypt.database.AccountDao;
 import com.majoinen.d.pwcrypt.input.InputStyle;
+import com.majoinen.d.pwcrypt.log.LogManager;
+import com.majoinen.d.pwcrypt.log.Logger;
 import com.majoinen.d.pwcrypt.views.login.LoginPresenter;
 
 /**
@@ -9,6 +17,9 @@ import com.majoinen.d.pwcrypt.views.login.LoginPresenter;
  */
 public class LoginController {
 
+    private static final Logger LOGGER =
+      LogManager.getLogger(LoginController.class);
+
     private LoginPresenter view;
 
     public void setView(LoginPresenter view) {
@@ -16,6 +27,44 @@ public class LoginController {
     }
 
     public void login(String email, String password) {
+        DatabaseController databaseController;
+        try {
+            databaseController = DatabaseControllerFactory.getController(
+              email, PwCrypt.DATABASE_CONFIG);
+            if(!databaseController.databaseExists()) {
+                LOGGER.debug("Database does not exist");
+                failedLogin();
+            } else {
+                attemptLogin(email, password, AccountDao.getInstance(
+                  databaseController));
+            }
+        } catch(DBUtilsException e) {
+            LOGGER.error("Error logging in", e);
+            failedLogin(); // TODO: Change to error alert
+        }
+    }
+
+    private void attemptLogin(String email, String password,
+      AccountDao accountDao) {
+        LOGGER.info("Attempting login");
+        try {
+            if(accountDao.authenticateUser(email, password.toCharArray()))
+                successfulLogin();
+            else {
+                LOGGER.info("Incorrect credentials");
+                failedLogin();
+            }
+        } catch(DBUtilsException | EncryptionUtilsException e) {
+            LOGGER.error("Error attempting login", e);
+        }
+    }
+
+    private void successfulLogin() {
+        LOGGER.info("Successful login");
+        PwCrypt.getInstance().showMessage("Successfully logged in!");
+    }
+
+    private void failedLogin() {
         view.styleEmailInput(InputStyle.ERROR);
         view.stylePasswordInput(InputStyle.ERROR);
         view.styleLoginButton(InputStyle.ERROR);
