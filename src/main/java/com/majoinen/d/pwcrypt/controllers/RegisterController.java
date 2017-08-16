f8b4ccfabb7fe82c6e33a53c172ae38837a6a324
Daniel Majoinen <daniel@majoinen.com>
2017-08-16 23:48:00 +1000

    Abstract validation to RegisterController

--- src/main/java/com/majoinen/d/pwcrypt/controllers/RegisterController.java
+++ src/main/java/com/majoinen/d/pwcrypt/controllers/RegisterController.java
@@ -1,5 +1,10 @@
 package com.majoinen.d.pwcrypt.controllers;
 
+import com.majoinen.d.pwcrypt.input.EmailValidator;
+import com.majoinen.d.pwcrypt.input.InputStyle;
+import com.majoinen.d.pwcrypt.input.ValidationResult;
+import com.majoinen.d.pwcrypt.log.LogManager;
+import com.majoinen.d.pwcrypt.log.Logger;
 import com.majoinen.d.pwcrypt.views.register.RegisterPresenter;
 
 /**
@@ -8,9 +13,37 @@ import com.majoinen.d.pwcrypt.views.register.RegisterPresenter;
  */
 public class RegisterController {
 
+    private static final Logger LOGGER =
+      LogManager.getLogger(RegisterController.class);
+
     private RegisterPresenter view;
 
     public void setView(RegisterPresenter view) {
         this.view = view;
+    }
+
+    public void register(String email, String password) {
+        if(validateEmail(email) & validatePassword(password))
+            LOGGER.info("Registration successful");
+    }
+
+    private boolean validateEmail(String email) {
+        String text = email.replaceAll("\\s","").toLowerCase();
+        ValidationResult result = EmailValidator.validate(text);
+        if(!result.isValid()) {
+            view.styleEmailInput(InputStyle.ERROR);
+            view.setEmailErrorLabel(result.getReason());
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validatePassword(String password) {
+        if(password.length() == 0) {
+            view.stylePasswordInput(InputStyle.ERROR);
+            view.setPasswordErrorLabel("Password is required");
+            return false;
+        }
+        return true;
     }
 }
--- src/main/java/com/majoinen/d/pwcrypt/views/register/RegisterPresenter.java
+++ src/main/java/com/majoinen/d/pwcrypt/views/register/RegisterPresenter.java
@@ -4,9 +4,8 @@ import com.gluonhq.charm.glisten.afterburner.GluonPresenter;
 import com.gluonhq.charm.glisten.mvc.View;
 import com.majoinen.d.pwcrypt.PwCrypt;
 import com.majoinen.d.pwcrypt.controllers.RegisterController;
-import com.majoinen.d.pwcrypt.input.EmailValidator;
-import com.majoinen.d.pwcrypt.views.StyleManager;
-import com.majoinen.d.pwcrypt.input.ValidationResult;
+import com.majoinen.d.pwcrypt.input.InputStyle;
+import com.majoinen.d.pwcrypt.input.InputStyleManager;
 import com.majoinen.d.pwcrypt.log.LogManager;
 import com.majoinen.d.pwcrypt.log.Logger;
 import com.majoinen.d.pwcrypt.views.ViewManager;
@@ -20,7 +19,7 @@ import javax.inject.Inject;
 
 public class RegisterPresenter extends GluonPresenter<PwCrypt> {
 
-    private static final Logger logger =
+    private static final Logger LOGGER =
       LogManager.getLogger(RegisterPresenter.class);
 
     @FXML
@@ -30,10 +29,16 @@ public class RegisterPresenter extends GluonPresenter<PwCrypt> {
     private TextField emailInput;
 
     @FXML
+    private Label emailLabel;
+
+    @FXML
     private Label emailErrorLabel;
 
     @FXML
     private PasswordField passwordInput;
+
+    @FXML
+    private Label passwordLabel;
 
     @FXML
     private Label passwordErrorLabel;
@@ -42,23 +47,21 @@ public class RegisterPresenter extends GluonPresenter<PwCrypt> {
     private Button register;
 
     @Inject
-    private RegisterController registerController;
+    private RegisterController controller;
 
     public void initialize() {
-        registerController.setView(this);
+        controller.setView(this);
         // Hide the AppBar
         view.setOnShowing(event ->
           PwCrypt.getInstance().getAppBar().setVisible(false));
 
-        initTextField(emailInput, emailErrorLabel);
-        initTextField(passwordInput, passwordErrorLabel);
+        initTextField(emailInput, emailLabel, emailErrorLabel);
+        initTextField(passwordInput, passwordLabel, passwordErrorLabel);
     }
 
     @FXML
     private void register() {
-        if(validateEmail() & validatePassword()) {
-            logger.info("Register success");
-        }
+        controller.register(emailInput.getText(), passwordInput.getText());
     }
 
     @FXML
@@ -68,39 +71,33 @@ public class RegisterPresenter extends GluonPresenter<PwCrypt> {
         ViewManager.LOGIN_VIEW.switchView();
     }
 
-    private void initTextField(TextField textField, Label errorLabel) {
+    private void initTextField(TextField textField, Label label,
+      Label errorLabel) {
         textField.textProperty().addListener((obs, ov, nv) -> {
-            StyleManager.styleInputNormal(textField);
+            InputStyleManager.styleInputNormal(textField, label);
             errorLabel.setText(null);
         });
+    }
+
+    public void styleEmailInput(InputStyle style) {
+        InputStyleManager.styleInput(emailInput, emailLabel, style);
+    }
+
+    public void setEmailErrorLabel(String text) {
+        emailErrorLabel.setText(text);
+    }
+
+    public void stylePasswordInput(InputStyle style) {
+        InputStyleManager.styleInput(passwordInput, passwordLabel, style);
+    }
+
+    public void setPasswordErrorLabel(String text) {
+        passwordErrorLabel.setText(text);
     }
 
     private void clear(TextField textField, Label errorLabel) {
         textField.clear();
         errorLabel.setText(null);
-        StyleManager.styleInputNormal(textField);
-    }
-
-    private boolean validateEmail() {
-        String email = emailInput.getText().replaceAll("\\s","").toLowerCase();
-        ValidationResult result = EmailValidator.validate(email);
-        if(!result.isValid()) {
-            StyleManager.styleInputError(emailInput);
-            emailErrorLabel.setText(result.getReason());
-            return false;
-        }
-        StyleManager.styleInputNormal(emailInput);
-        return true;
-    }
-
-    private boolean validatePassword() {
-        if(passwordInput.getText().length() == 0) {
-            StyleManager.styleInputError(passwordInput);
-            passwordErrorLabel.setText("Password required");
-            return false;
-        } else {
-            StyleManager.styleInputNormal(passwordInput);
-            return true;
-        }
+        InputStyleManager.styleInputNormal(textField, errorLabel);
     }
 }
