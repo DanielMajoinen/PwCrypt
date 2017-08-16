7f9e48a13b2ac108535808c9dc8ce9dc7803bfa8
Daniel Majoinen <daniel@majoinen.com>
2017-08-16 23:46:14 +1000

    Add labels to styling input fields

---
+++ src/main/java/com/majoinen/d/pwcrypt/input/InputStyleManager.java
@@ -0,0 +1,35 @@
+package com.majoinen.d.pwcrypt.input;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ * @author Daniel Majoinen
+ * @version 1.0, 16/8/17
+ */
+public class InputStyleManager {
+
+    private InputStyleManager() { }
+
+    public static void styleInput(TextField textField, Label label,
+      InputStyle style) {
+        if(style.equals(InputStyle.NORMAL))
+            styleInputNormal(textField, label);
+        else if(style.equals(InputStyle.ERROR))
+            styleInputError(textField, label);
+    }
+
+    public static void styleInputNormal(TextField textField, Label label) {
+        textField.getStyleClass().remove("pwcrypt-input-error");
+        textField.getStyleClass().add("pwcrypt-input");
+        label.getStyleClass().remove("pwcrypt-label-error");
+        label.getStyleClass().add("pwcrypt-label");
+    }
+
+    public static void styleInputError(TextField textField, Label label) {
+        textField.getStyleClass().remove("pwcrypt-input");
+        textField.getStyleClass().add("pwcrypt-input-error");
+        label.getStyleClass().remove("pwcrypt-label");
+        label.getStyleClass().add("pwcrypt-label-error");
+    }
+}
