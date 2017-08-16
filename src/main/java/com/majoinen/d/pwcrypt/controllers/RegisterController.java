ce8444d47231eff1d347117a7a8add8feb5c88e3
Daniel Majoinen <daniel@majoinen.com>
2017-08-17 02:44:49 +1000

    Add ability to register an account in the database

--- src/main/java/com/majoinen/d/pwcrypt/controllers/RegisterController.java
+++ src/main/java/com/majoinen/d/pwcrypt/controllers/RegisterController.java
@@ -1,10 +1,18 @@
 package com.majoinen.d.pwcrypt.controllers;
 
+import com.majoinen.d.database.DatabaseController;
+import com.majoinen.d.database.DatabaseControllerFactory;
+import com.majoinen.d.database.exception.DBUtilsException;
+import com.majoinen.d.encryption.exception.EncryptionUtilsException;
+import com.majoinen.d.pwcrypt.PwCrypt;
+import com.majoinen.d.pwcrypt.database.AccountDao;
+import com.majoinen.d.pwcrypt.exception.PwCryptException;
 import com.majoinen.d.pwcrypt.input.EmailValidator;
 import com.majoinen.d.pwcrypt.input.InputStyle;
 import com.majoinen.d.pwcrypt.input.ValidationResult;
 import com.majoinen.d.pwcrypt.log.LogManager;
 import com.majoinen.d.pwcrypt.log.Logger;
+import com.majoinen.d.pwcrypt.util.Path;
 import com.majoinen.d.pwcrypt.views.register.RegisterPresenter;
 
 /**
@@ -23,8 +31,43 @@ public class RegisterController {
     }
 
     public void register(String email, String password) {
-        if(validateEmail(email) & validatePassword(password))
-            LOGGER.info("Registration successful");
+        if(validateEmail(email) & validatePassword(password)) {
+            try {
+                DatabaseController databaseController =
+                  DatabaseControllerFactory.getController(email,
+                    PwCrypt.DATABASE_CONFIG);
+                databaseController.setProperty("root.directory",
+                  Path.File.privateStorage());
+                if(!databaseController.databaseExists())
+                    initNewAccount(email, password, databaseController);
+                else
+                    accountAlreadyExists();
+            } catch(DBUtilsException e) {
+                LOGGER.error("Error checking for database file", e);
+            } catch(PwCryptException e) {
+                LOGGER.error("Error setting database root directory", e);
+            }
+        }
+    }
+
+    private void initNewAccount(String email, String password,
+      DatabaseController databaseController) {
+        try {
+            databaseController.init();
+            if(AccountDao.getInstance(databaseController)
+              .addNewUser(email, password.toCharArray())) {
+                LOGGER.info("Registration successful");
+                PwCrypt.getInstance().showMessage("Successfully registered!");
+            }
+        } catch(DBUtilsException | EncryptionUtilsException e) {
+            LOGGER.error("Error creating new account", e);
+        }
+    }
+
+    private void accountAlreadyExists() {
+        view.styleEmailInput(InputStyle.ERROR);
+        view.stylePasswordInput(InputStyle.ERROR);
+        view.setEmailErrorLabel("Email already in use");
     }
 
     private boolean validateEmail(String email) {
